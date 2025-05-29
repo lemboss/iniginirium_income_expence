bd68f2f06412cbb76bea6d1803534db87c11886a
lemboss <69087893+lemboss@users.noreply.github.com>
2025-05-29 12:41:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from bot import bot
 from handlers.user_handlers import register_user_handlers
 from handlers.move_handlers import register_move_handlers
@@ -7,10 +8,12 @@ register_user_handlers(bot)
 register_move_handlers(bot)
 register_report_handlers(bot)
 
-bot.delete_webhook(drop_pending_updates=True)
-bot.polling(skip_pending=True)
-
-
-
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.delete_webhook(drop_pending_updates=True)
+            bot.polling(none_stop=True)
+        except:
+            sleep(1)
 
     
